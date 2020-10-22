f60dedce991f02ab51ede16c04cbb78cb6a5b66a
Trent Smith <trentnsmith@gmail.com>
2020-10-21 21:03:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='App'>
+      <section className="temp-title">
+        <h1>Coming Soon</h1>
+      </section>
+      <section className="ww1">
+        <div className="temp-picture">
+          
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;

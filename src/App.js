99046a4bb969606bcc9a3d4a0156f1913519d48c
Trent Smith <trentnsmith@gmail.com>
2020-10-22 21:22:30 -0500

    complete placeholder info

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <main className='App'>
-      <section className="temp-title">
-        <h1>Coming Soon</h1>
-      </section>
-      <section className="ww1">
-        <div className="temp-picture">
-          
-        </div>
-      </section>
-    </main>
-  );
-}
+class App extends Component {
+  render () {
+    return (
+      <main className='App'>
+        <section className="temp-title">
+          <h1>Coming Soon...</h1>
+        </section>
+        <section className="ww1">
+          <div className="ww1-a">
+            <img 
+              ids="image" 
+              src="/pictures/wedding.jpg" 
+              alt="jltrent"
+            ></img>
+          </div>
+          <div>
+            <h2>Save the Date!</h2>
+            <h3>8/21/21</h3>
+          </div>
+        </section>
+      </main>
+    );
+  };
+};
 
 export default App;
